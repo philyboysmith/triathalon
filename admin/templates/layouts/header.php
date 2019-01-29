b708b15ecd1db318fbdce76738b0a31b9befbb8c
Phil Smith <phil@amillionmonkeys.co.uk>
2019-01-29 13:40:59 +0000

    initial commit

--- admin/templates/layouts/header.php
+++ admin/templates/layouts/header.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title><?php perch_pages_title(); ?> | Duchenne Dash 2018</title>
+    <title><?php perch_pages_title(); ?> | Duchenne Triathalon 2019</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- favicon -->
