58ed60edb46b014a85fb60c3ed92577e5b19c407
Phil Smith <phil@amillionmonkeys.co.uk>
2019-01-29 13:15:37 +0000

    initial commit

--- admin/config/config.production.php
+++ admin/config/config.production.php
@@ -4,8 +4,8 @@
 
     define('PERCH_SCHEDULE_SECRET', 'XbXU0zOkH7jq6imc');
 
-    define('PERCH_DB_USERNAME', 'root');
-    define('PERCH_DB_PASSWORD', 'bc2faa52ff21ff84a5b2fafa8687a06802e6c366c686d544');
+    define('PERCH_DB_USERNAME', 'forge');
+    define('PERCH_DB_PASSWORD', '3MfLdqsIKlEvJBO7Dcb5');
     define('PERCH_DB_SERVER', "localhost");
     define('PERCH_DB_DATABASE', 'triathalon');
     define('PERCH_DB_PREFIX', 'perch3_');
